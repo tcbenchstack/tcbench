c186e266afa06402ece1e4ef946dc923774b5c01
Alessandro Finamore <finale80@gmail.com>
2024-09-08 18:19:30 +0200

    added .tcbenchrc and associated class object

--- src/tcbench/__init__.py
+++ src/tcbench/__init__.py
@@ -1,6 +1,49 @@
+from collections import UserDict
+
 import pathlib
+import os.path
+import yaml
+
+from tcbench.libtcdatasets.constants import DATASETS_DEFAULT_INSTALL_ROOT_FOLDER
+from tcbench.libtcdatasets import fileutils
 
 __version__ = "0.0.22"
+
+TCBENCHRC_PATH = pathlib.Path(os.path.expandvars("$HOME")) / ".tcbenchrc"
+
+class TCBenchConfig(UserDict):
+    def __init__(self):
+        super().__init__()
+        if not TCBENCHRC_PATH.exists():
+            TCBenchConfig.init()
+        self.load()
+
+    @classmethod
+    def init(cls):
+        data=dict(
+            datasets=dict(
+                install_folder=str(DATASETS_DEFAULT_INSTALL_ROOT_FOLDER)
+            )
+        )
+        fileutils.save_yaml(data, TCBENCHRC_PATH)
+        
+    @property
+    def install_folder(self):
+        return pathlib.Path(self.data["datasets"]["install_folder"])
+
+    def save(self):
+        fileutils.save_yaml(self.data, TCBENCHRC_PATH)
+
+    def load(self):
+        self.data = fileutils.load_yaml(TCBENCHRC_PATH)
+
+_tcbenchrc = TCBenchConfig()
+
+
+##########################
+# OLDER CONTENT
+##########################
+
 
 DEFAULT_AIM_REPO = pathlib.Path("./aim-repo")
 DEFAULT_ARTIFACTS_FOLDER = pathlib.Path("./aim-repo/artifacts")
