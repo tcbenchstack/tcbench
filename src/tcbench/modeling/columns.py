0e1668ade53f8f65c8fed815a26f57c2b2a114f5
Alessandro Finamore <finale80@gmail.com>
2024-09-14 15:46:14 +0200

    added generic columns name module

---
+++ src/tcbench/modeling/columns.py
@@ -0,0 +1,15 @@
+# data splits
+SPLIT_NAME_TRAIN = "train"
+SPLIT_NAME_TEST = "test"
+COL_SPLIT_NAME = "split"
+COL_SPLIT_INDEX = "split_index"
+COL_SPLIT_TRAIN_INDICES = "split_train_indices"
+COL_SPLIT_TEST_INDICES = "split_test_indices"
+
+# datasets
+COL_APP = "app"
+COL_PKTS_SIZE = "pkts_size"
+COL_PKTS_DIR = "pkts_dir"
+COL_BYTES = "bytes"
+COL_PACKETS = "packets"
+COL_ROW_ID = "row_id"
