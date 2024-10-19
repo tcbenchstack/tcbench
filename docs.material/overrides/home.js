4fc576f561ae74c387fa6b3cc8c4c0e2d5c6643c
Alessandro Finamore <finale80@gmail.com>
2024-10-19 21:45:55 +0200

    bugfix: restoring home page nagivation broken to package version dependency

--- docs.material/overrides/home.js
+++ docs.material/overrides/home.js
@@ -2,7 +2,8 @@ import * as PIXI from "https://cdn.skypack.dev/pixi.js@5.x";
 import { KawaseBlurFilter } from "https://cdn.skypack.dev/@pixi/filter-kawase-blur@3.2.0";
 import SimplexNoise from "https://cdn.skypack.dev/simplex-noise@3.0.0";
 import hsl from "https://cdn.skypack.dev/hsl-to-hex";
-import debounce from "https://cdn.skypack.dev/debounce";
+//import debounce from "https://cdn.skypack.dev/debounce";
+import debounce from "https://cdn.skypack.dev/new/debounce@v2.1.0/dist=es2019"
 
 // return a random number within a range
 function random(min, max) {
